f4044f62ff501c28beee55ceab31a436858b8e2a
oneparalyzer <103250853+oneparalyzer@users.noreply.github.com>
2022-11-14 18:06:31 +0300

    Готово

--- FilmFeedback/Controllers/HomeController.cs
+++ FilmFeedback/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using FilmFeedback.Models;
+﻿using FilmFeedback.Data;
+using FilmFeedback.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,12 +8,15 @@ namespace FilmFeedback.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
